1f14647f861952c0a504b0453d3c171a45e3181f
1emonislam <emonislamweb@gmail.com>
2022-03-06 17:16:40 +0600

    ok

--- src/components/config/Player.js
+++ src/components/config/Player.js
@@ -203,7 +203,7 @@ function Player({ url }) {
     };
 
     const handleSeekChange = (e, newValue) => {
-        console.log({ newValue });
+        // console.log({ newValue });
         setState({ ...state, played: parseFloat(newValue / 100) });
     };
 
@@ -212,7 +212,7 @@ function Player({ url }) {
     };
 
     const handleSeekMouseUp = (e, newValue) => {
-        console.log({ value: e.target });
+        // console.log({ value: e.target });
         setState({ ...state, seeking: false });
         // console.log(sliderRef.current.value)
         playerRef.current.seekTo(newValue / 100, "fraction");
